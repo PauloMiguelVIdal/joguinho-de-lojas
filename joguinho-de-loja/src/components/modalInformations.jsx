7f50002f937ac6a1d8c0d84808efb75a94dc465c
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-04-14 22:28:09 -0300

    criar separações

---
+++ joguinho-de-loja/src/components/modalInformations.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function modalInformations() {
+
+const information = {
+"funcionários":"totalfuncionários",
+"custo funcionário mínimo": "custoMínimoFuncionário",
+"custo funcionário máximo": "custoMáximoFuncionário",
+"imposto":"% de imposto",
+"imposto fixo":"porcentagem imposto fixo",
+"Faturamento mínimo":"Faturamenot Minimo",
+"faturamento máximo":"Faturamenot máximo"
+}
+
+
+
+
+  return (
+    <div>modalInformations</div>
+  )
+}
