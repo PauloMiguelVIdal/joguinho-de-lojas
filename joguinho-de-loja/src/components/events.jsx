3726bdb40bdaecd0d062eff1874d4dc837231daa
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-04-18 23:03:53 -0300

    ajustado funções de compra

--- joguinho-de-loja/src/components/events.jsx
+++ joguinho-de-loja/src/components/events.jsx
@@ -1,82 +1,41 @@
 import React, { useState } from 'react';
 
-const Events = (props) => {
-
+const Events = () => {
 
   const centralResultado = () => {
-    const selecionarLojaNova = () => {
-      const todasLojas = ["lojas pequenas", "lojas médias", "lojas grandes"];
-      const parametrosLojaSelecionada = Math.floor(Math.random() * todasLojas.length);
-      const novaLojaSelecionada = todasLojas[parametrosLojaSelecionada];
-      return novaLojaSelecionada;
-    }
+    const selecionarItem = (lista) => lista[Math.floor(Math.random() * lista.length)];
+    const todasLojas = ["lojas pequenas", "lojas médias", "lojas grandes"];
+    const situação = ["aumento", "queda"];
+    const porcentagem = [1, 3, 5, 7, 10, 15, 20, 30];
+    const periodo = [3, 7, 15, 30];
 
-    const novaLojaSelecionada = selecionarLojaNova();
-
-    const selecionarSituação = () => {
-      const situação = ["aumento", "queda"];
-      const parametrosSituaçãoSelecionada = Math.floor(Math.random() * situação.length);
-      const novaSituaçãoSelecionada = situação[parametrosSituaçãoSelecionada];
-      return novaSituaçãoSelecionada;
-    }
-
-    const novaSituaçãoSelecionada = selecionarSituação();
-
-
-    const selecionarPorcentagem = () => {
-      const porcentagem = [1, 3, 5, 7, 10, 15, 20, 30];
-      const parametrosPorcentagemSelecionada = Math.floor(Math.random() * porcentagem.length);
-      const novaPorcentagemSelecionada = porcentagem[parametrosPorcentagemSelecionada];
-      return novaPorcentagemSelecionada;
-    }
-
-    const novaPorcentagemSelecionada = selecionarPorcentagem();
-
-    const selecionarPeriodo = () => {
-      const periodo = [3, 7, 15, 30];
-      const parametrosPeriodoSelecionado = Math.floor(Math.random() * periodo.length);
-      const novoPeriodoSelecionado = periodo[parametrosPeriodoSelecionado];
-      return novoPeriodoSelecionado;
-    }
-
-    const novoPeriodoSelecionado = selecionarPeriodo();
-
-
-
-
-
-    const evento = {
-      "evento": `As ${novaLojaSelecionada} terão ${novaSituaçãoSelecionada} de faturamento de ${novaPorcentagemSelecionada}% durante o periodo de ${novoPeriodoSelecionado} dias`,
-      "LojaSelecionada": novaLojaSelecionada,
-      "situaçãoSelecionada": novaSituaçãoSelecionada,
-      "porcentagemSelecionada": novaPorcentagemSelecionada,
-      "periodoSelecionado": novoPeriodoSelecionado
-    }
-
-    return evento;
+    return {
+      evento: `As ${selecionarItem(todasLojas)} terão ${selecionarItem(situação)} de faturamento de ${selecionarItem(porcentagem)}% durante o período de ${selecionarItem(periodo)} dias`,
+      LojaSelecionada: selecionarItem(todasLojas),
+      situaçãoSelecionada: selecionarItem(situação),
+      porcentagemSelecionada: selecionarItem(porcentagem),
+      periodoSelecionado: selecionarItem(periodo)
+    };
   }
 
+  const [eventoAtual, setEventoAtual] = useState("");
 
-  const [eventoAtual,setEventoAtual] = useState("")
-
-  
   const testar = () => {
     const evento = centralResultado();
     console.log(evento);
-    return evento
+    return evento;
   }
 
-const testandoTudo = () =>{
-  const novoEvento = testar()
-   setEventoAtual(novoEvento)
-   console.log(eventoAtual)
-}
-
+  const testandoTudo = () => {
+    const novoEvento = testar();
+    setEventoAtual(novoEvento);
+    console.log(eventoAtual);
+  }
 
   return (
     <div className='bg-black flex justify-center items-center z-20'>
       <button onClick={testandoTudo} className='bg-white'>sortear</button>
-      <p>Este será o tipo de evento selecionado: </p>
+      <p>Este será o tipo de evento selecionado: {eventoAtual.evento}</p>
     </div>
   );
 
--- joguinho-de-loja/src/components/nextDay.jsx
+++ joguinho-de-loja/src/components/nextDay.jsx
@@ -9,63 +9,77 @@ export default function NextDay() {
 
     const ProximoDia = () => {
         if (dados.dia % 30 === 0) {
-            atualizarDados({...dados, despesas: { ...dados.despesas, despesasPagas: false }});
+            atualizarDados({ ...dados, despesas: { ...dados.despesas, despesasPagas: false } });
             if (!dados.despesas.despesasPagas) {
-                return AtualizarEstadoModal(true);
+                return atualizarDados({...dados, estadoModal:true});
             }
         }
-    
+
+
+
+
+        const chanceSorteado = 3
+
+        const realizarSorterio = () => {
+            const resultado = Math.random() * 100
+            if (resultado <= chanceSorteado) {
+                console.log("sorteio ocorreu")
+            }
+        }
+
+
+
         const novoDia = dados.dia + 1;
         const saldoAtualizado =
             dados.saldo +
             dados.terrenos.quantidade * dados.terrenos.faturamentoUnitário +
             dados.lojasP.quantidade * dados.lojasP.faturamentoUnitário +
             dados.lojasM.quantidade * dados.lojasM.faturamentoUnitário +
-            dados.lojasG.quantidade * dados.lojasG.faturamentoUnitário 
+            dados.lojasG.quantidade * dados.lojasG.faturamentoUnitário
 
-            // dados.terrenos.quantidade * 100 -
-            // dados.lojasP.quantidade * 250 -
-            // dados.lojasM.quantidade * 400 -
-            // dados.lojasG.quantidade * 750;
-    
+        // dados.terrenos.quantidade * 100 -
+        // dados.lojasP.quantidade * 250 -
+        // dados.lojasM.quantidade * 400 -
+        // dados.lojasG.quantidade * 750;
+
         atualizarDados(['dia'], novoDia);
         atualizarDados(['saldo'], saldoAtualizado);
-
+        realizarSorterio()
         gerarFaturamentoTerrenos();
         gerarFaturamentoLojasP();
         gerarFaturamentoLojasM();
         gerarFaturamentoLojasG();
     };
-    
+
     const PagarDespesas = () => {
         if (dados.despesas.despesasPagas) {
             return alert("Despesas desse mês já foram pagas.");
         } else {
             const novoSaldo = dados.saldo - dados.despesas.despesasLojasP - dados.despesas.despesasLojasM - dados.despesas.despesasLojasG;
-            atualizarDados({...dados, saldo: novoSaldo, despesas: {...dados.despesas, despesasPagas: true}});
+            atualizarDados({ ...dados, saldo: novoSaldo, despesas: { ...dados.despesas, despesasPagas: true } });
             alert("Despesas pagas.");
         }
     };
     const gerarFaturamentoTerrenos = () => {
         const novoFatuUnitárioTerreno = Math.floor(Math.random() * (dados.terrenos.faturamentoMáximo - dados.terrenos.faturamentoMínimo + 1)) + dados.terrenos.faturamentoMínimo;
-        atualizarDados({...dados, terrenos: {...dados.terrenos, faturamentoUnitário: novoFatuUnitárioTerreno}});
+        atualizarDados({ ...dados, terrenos: { ...dados.terrenos, faturamentoUnitário: novoFatuUnitárioTerreno } });
     };
-    
+
     const gerarFaturamentoLojasP = () => {
         const novoFatuUnitárioLojaP = Math.floor(Math.random() * (dados.lojasP.faturamentoMáximo - dados.lojasP.faturamentoMínimo + 1)) + dados.lojasP.faturamentoMínimo;
-        atualizarDados({...dados, lojasP: {...dados.lojasP, faturamentoUnitário: novoFatuUnitárioLojaP}});
+        atualizarDados({ ...dados, lojasP: { ...dados.lojasP, faturamentoUnitário: novoFatuUnitárioLojaP } });
     };
-    
+
     const gerarFaturamentoLojasM = () => {
         const novoFatuUnitárioLojaM = Math.floor(Math.random() * (dados.lojasM.faturamentoMáximo - dados.lojasM.faturamentoMínimo + 1)) + dados.lojasM.faturamentoMínimo;
-        atualizarDados({...dados, lojasM: {...dados.lojasM, faturamentoUnitário: novoFatuUnitárioLojaM}});
+        atualizarDados({ ...dados, lojasM: { ...dados.lojasM, faturamentoUnitário: novoFatuUnitárioLojaM } });
     };
-    
+
     const gerarFaturamentoLojasG = () => {
         const novoFatuUnitárioLojaG = Math.floor(Math.random() * (dados.lojasG.faturamentoMáximo - dados.lojasG.faturamentoMínimo + 1)) + dados.lojasG.faturamentoMínimo;
-        atualizarDados({...dados, lojasG: {...dados.lojasG, faturamentoUnitário: novoFatuUnitárioLojaG}});
+        atualizarDados({ ...dados, lojasG: { ...dados.lojasG, faturamentoUnitário: novoFatuUnitárioLojaG } });
     };
-    
+
     return (
 
         <div className="grid col-start-1 col-end-3 row-2">
