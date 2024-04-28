52733baad2881e9e9570a94a7d01c95a0b97f157
Paulo Miguel <paulo2.0miguel@gmail.com>
2024-04-27 22:20:11 -0300

    criando o sistema de eventos

--- joguinho-de-loja/src/components/events.jsx
+++ joguinho-de-loja/src/components/events.jsx
@@ -9,6 +9,12 @@ const Events = () => {
 
 
 
+
+
+
+
+
+
   useEffect(() => {
     // Verifica se é necessário atualizar as despesas e o estado modal
     if (dados.dia % 30 === 0 && !dados.despesas.despesasPagas) {
@@ -37,13 +43,65 @@ const Events = () => {
 
 
   const centralResultado = () => {
+    
+    
+    
+    
+    
+    
+    
+    
     const selecionarItem = (lista) => lista[Math.floor(Math.random() * lista.length)];
     const todasLojas = ["lojas pequenas", "lojas médias", "lojas grandes"];
     const situacao = ["aumento", "queda"];
     const porcentagem = [1, 3, 5, 7, 10, 15, 20, 30];
     const periodo = [3, 7, 15, 30];
-  
+    //
+    const modalEvents = ["modalDespesas","modalFaturamento","modalImpostos","modalFuncionários"]
+    //
+    
+    if(modalEvents == "modalDespesas"){
+
+    }else if(modalEvents == "modalFaturamento"){
+
+    }else if(modalEvents == "modalImpostos"){
+
+    }else if(modalEvents == "modalFuncionários"){
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const modalFuncionários = {
+     custo :dados.funcionários.custo,
+     custoMínimo :dados.funcionários.custoMínimo,
+     custoMáximo :dados.funcionários.custoMáximo,
+    }
+
+    const eventosCustoFunc = ["custoMínimoFunc","custoMáximoFunc"]
+    
+
     const novoEvento = {};
+    const novoCustoFunc = {};
+    novoCustoFunc.title = selecionarItem(todasLojas);
+    novoCustoFunc.custo = selecionarItem(todasLojas);
+    novoCustoFunc.custoMínimo = selecionarItem(todasLojas);
+    novoCustoFunc.custoMáximo = selecionarItem(todasLojas);
+
+
+
   
     novoEvento.title = `As ${selecionarItem(todasLojas)} terão ${selecionarItem(situacao)} de faturamento de ${selecionarItem(porcentagem)}% durante o período de ${selecionarItem(periodo)} dias`;
     novoEvento.LojaSelecionada = selecionarItem(todasLojas);
@@ -55,14 +113,17 @@ const Events = () => {
   };
   
   return (
-    <div className='bg-black flex justify-center items-center z-20'>
-      <button onClick={() =>{
-         atualizarDados('iniciarSorteio', true)
-    console.log(dados.modal.estadoModal)
+    <div>
 
-  }
+    {/* <div className='bg-black flex justify-center items-center z-20'>
+      <button onClick={() =>{
+        atualizarDados('iniciarSorteio', true)
+        console.log(dados.modal.estadoModal)
+        
+      }
     } className='bg-white'>Sortear</button>
       <p className='text-white'>Este será o tipo de evento selecionado: {dados.eventoAtual.title}</p>
+    </div> */}
     </div>
   );
 };
