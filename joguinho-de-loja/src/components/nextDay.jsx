ab137482e0b7be44d46de9f390b4b63a9e432de3
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-01-07 16:13:13 -0300

    resovido conflitos

--- joguinho-de-loja/src/components/nextDay.jsx
+++ joguinho-de-loja/src/components/nextDay.jsx
@@ -112,13 +112,8 @@ export default function NextDay() {
                 <button className="w-[100px] h-[100px] bg-laranja rounded-[20px] flex items-center justify-center mr-[10px]" onClick={ProximoDia}>
                     <img className="w-[72px] h-[72px]" src={PróximoImg} />
                 </button>
-
-                <button className="w-[100px] h-[100px] bg-laranja rounded-[20px] flex items-center justify-center ml-[10px]"
-                    onClick={PagarDespesas}><img className="w-[72px] h-[72px]" src={despesasImg} /></button>
-
              
-
             </div>
-
+        </div>
     )
 }
