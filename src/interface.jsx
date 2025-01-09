b468c1a0a477a1f08c819eba225873ebbcd7827f
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-01-09 09:59:17 -0300

    criado component Buttons

--- src/interface.jsx
+++ src/interface.jsx
@@ -1,23 +1,22 @@
 import React, { useContext, useState } from "react";
 import { CentraldeDadosContext } from "./centralDeDadosContext";
-import NextDay from "./components/nextDay";
+
 import Buy from "./components/buy";
 import Notificação from "./notificação";
 import Events from "./components/events";
 import Employees from "./components/employees";
 import Texes from "./components/Texes";
-import PayTexes from "./components/PayTexes";
+;
 import Sorteio from "./components/Sorteio";
 import ActiveEvents from "./components/ActiveEvents";
-
+import Buttons from "./components/Buttons";
 function Interface() {
     const { dados, atualizarDados } = useContext(CentraldeDadosContext)
 
     return (
-        <div className="w-[100vw] h-[100vh] bg-gradient-to-b from-roxo to-roxoForte grid grid-rows-2 grid-cols-2">
+        <div className="w-[100vw] h-[100vh] bg-[#6A00FF] flex justify-center items-center">
             <Notificação />
-            {/* <button className="bg-white w-[100px] h-[120px]" onClick={()=>AtualizarEstadoModal(true)}>abrir modal</button> */}
-
+<div className="w-[90vw] h-[90vh] rounded-[20px] bg-gradient-to-b from-roxo to-roxoForte grid grid-rows-2 grid-cols-2">
 
             <div className="flex flex-col align-center text-center justify-around absolute top-[20px] right-[20px]">
                 <div className="flex justify-between pr-[10px] pl-[10px] items-center w-[300px] h-[30px] rounded-[20px] bg-white box2">
@@ -30,74 +29,17 @@ function Interface() {
                     <h1 className="fonteBold text-roxo text-[20px]">{dados.dia}</h1>
                 </div>
             </div>
+
+<Buttons/>
+            <Buy />
             <div className="flex justify-between pr-[10px] pl-[10px] items-center w-[300px] h-[30px] rounded-[20px] bg-white box2 absolute top-[20px] left-[20px]">
                 <h1 className="fonteLight text-roxo text-[20px] ">Mercados Mandakaru</h1>
             </div>
-            {/* <div className="flex justify-evenly items-center w-[250px] h-[30px] rounded-[20px] bg-white box2">
-                    <h1 className="fonteLight text-roxo text-[20px]">faturamento Atual diário:</h1>
-                    <h1 className="fonteBold text-roxo text-[20px]">{dadosFaturamentoTotalLojasP + dadosFaturamentoTotalLojasM + dadosFaturamentoTotalLojasG}</h1>
-                </div> */}
-            {/* <div className="flex justify-evenly items-center w-[250px] h-[30px] rounded-[20px] bg-white box2">
-                    <h1 className="fonteLight text-roxo text-[20px]">Despesas do mês Atual:</h1>
-                    <h1 className="fonteBold text-roxo text-[20px]">gerar</h1>
-                </div> */}
-
-
-            {/* <h1>Preço terrenos:{dadosPreçosTerrenos}</h1>
-
-
-                        <h1>Preço construção LojaP:{dadosPreçosConstruçãoLojaP}</h1>
-
-                        
-                        <h1>Preço construção LojaM:{dadosPreçosConstruçãoLojaM}</h1>
-
-                        
-                        <h1>Preço construção LojaG:{dadosPreçosConstruçãoLojaG}</h1>
-                        
-
-                        <h1>Terrenos:{dadosTerrenos}</h1>
-
-                        
-                        <h1>lojas P:{dadosLojasP}</h1>
-                        
-
-                        <h1>lojas M:{dadosLojasM}</h1>
-                        
-
-                        <h1>lojas G:{dadosLojasG}</h1>
-                        
-
-                        <h1>custoFuncionário:{dadosCustoFuncionário}</h1>
-
-                        
-                        <h1>faturamentoUnitárioLojasP:{dadosFaturamentoUnitárioLojasP}</h1>
-                        
-                        
-                        <h1>faturamentoTotalLojasP:{dadosFaturamentoTotalLojasP}</h1>
-
-
-                        <h1>faturamentoUnitárioLojasM:{dadosFaturamentoUnitárioLojasM}</h1>
-
-
-                        <h1>faturamentoTotalLojasM:{dadosFaturamentoTotalLojasM}</h1>
-
-
-                        <h1>faturamentoUnitárioLojasG:{dadosFaturamentoUnitárioLojasG}</h1>
-                        
-                        
-                        <h1>faturamentoTotalLojasG:{dadosFaturamentoTotalLojasG}</h1> */}
-            <Buy />
-            <div className="grid col-start-1 col-end-3 row-2">
-                <div className="flex justify-center mt-[20px]">
-                    <NextDay />
-                    <PayTexes />
-                </div>
-            </div>
 
             <Events />
-            <div className="flex justify-start items-end ">
-
+                <div className="flex justify-start items-end ">
                 <ActiveEvents />
+                </div>
             </div>
         </div>
     )
