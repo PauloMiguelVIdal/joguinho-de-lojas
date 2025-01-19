b0948720070d51bb9d3e3255ef013eb3b2aef112
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-01-19 10:50:17 -0300

    feito a base da interface despesas

--- src/components/texes.jsx
+++ src/components/texes.jsx
@@ -1,57 +1,62 @@
 import React from 'react'
-import { useContext, useEffect} from 'react'
+import { useContext, useEffect } from 'react'
 import { CentraldeDadosContext } from '../centralDeDadosContext'
 import despesasImg from "../imagens/despesas.png";
 
 export default function Texes() {
 
-const {dados, atualizarDados} = useContext(CentraldeDadosContext)
+  const { dados, atualizarDados } = useContext(CentraldeDadosContext)
 
 
-// useEffect(() => {
-//   // Verifica se é necessário atualizar as despesas e o estado modal
-//   if (dados.dia % 30 === 0){
-//       atualizarDados({...dados.despesas,despesasPagas:false})
-//   }
-//     if (dados.dia % 30 === 0 && !dados.despesas.despesasPagas) {
-//       const novoEstado = { ...dados, modal: { ...dados.modal, estadoModal: true } };
-//       atualizarDados('despesas', novasDespesas);
-//       atualizarDados('modal', { ...dados.modal, estadoModal: true }); 
-//       // Chame o modelo de pagar dívidas aqui
-//     }
-//   }, [dados.dia, dados.despesas.despesasPagas]);
-
-
-
+  // useEffect(() => {
+  //   // Verifica se é necessário atualizar as despesas e o estado modal
+  //   if (dados.dia % 30 === 0){
+  //       atualizarDados({...dados.despesas,despesasPagas:false})
+  //   }
+  //     if (dados.dia % 30 === 0 && !dados.despesas.despesasPagas) {
+  //       const novoEstado = { ...dados, modal: { ...dados.modal, estadoModal: true } };
+  //       atualizarDados('despesas', novasDespesas);
+  //       atualizarDados('modal', { ...dados.modal, estadoModal: true }); 
+  //       // Chame o modelo de pagar dívidas aqui
+  //     }
+  //   }, [dados.dia, dados.despesas.despesasPagas]);
 
 
 
 
-const PagarDespesas = () => {
 
 
-  
-  if (dados.despesas.despesasPagas) {
+
+  const PagarDespesas = () => {
+
+
+
+    if (dados.despesas.despesasPagas) {
       return alert("Despesas desse mês já foram pagas.");
-  } else {
+    } else {
 
       const novoSaldo = dados.saldo - dados.despesas.despesasLojasP - dados.despesas.despesasLojasM - dados.despesas.despesasLojasG;
       // atualizarDados('saldo', novoSaldo);
       atualizarDados('despesas', { ...dados.despesas, despesasPagas: true });
-      
 
-      
+
+
       console.log(dados.saldo)
-       alert("Despesas pagas.");
+      alert("Despesas pagas.");
 
 
-  }
-};
+    }
+  };
 
   return (
     <div className='bg-[#6E0BF9] rounded-[40px]'>
-
-          {/* <button className="w-[100px] h-[100px] bg-laranja rounded-[20px] flex items-center justify-center ml-[10px]"
+      <h1>Despesas do mês</h1>
+      <h1>Imposto do mês</h1>
+      <h1>Custo funcionário</h1>
+      <h1>Total a pagar</h1>
+      <h1>Total faturado</h1>
+      <h1>Lucro líquido</h1>
+      {/* <button className="w-[100px] h-[100px] bg-laranja rounded-[20px] flex items-center justify-center ml-[10px]"
                     onClick={PagarDespesas}><img className="w-[72px] h-[72px]" src={despesasImg} /></button> */}
     </div>
   )
