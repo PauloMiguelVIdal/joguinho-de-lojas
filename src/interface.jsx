6bd20215a4da97bb01ef610c6d49b4d546ce29c8
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-02-05 15:28:02 -0300

    feito alterações na interface

--- src/interface.jsx
+++ src/interface.jsx
@@ -25,25 +25,25 @@ function Interface() {
                 {/* <div className="grid col-start-1 col-end-3 row-start-8 row-end-10">
                     <MoreOptions />
                 </div> */}
-                <div className="grid col-3  row-start-1 row-end-4">
+                <div className="grid col-start-3 col-end-4  row-start-1 row-end-4">
                     <Buttons />
                 </div>
-                <div className="grid col-start-4 col-end-9 row-start-1 row-end-10">
+                {/* <div className="grid col-start-4 col-end-9 row-start-1 row-end-10">
                     <ButtonChange />
-                </div>
+                </div> */}
                 <div className="grid col-start-1 col-end-3 row-start-1 row-end-10">
                     <Buy />
                 </div>
-                <div className="grid col-start-3 col-end-7 row-start-4 row-end-9">
+                <div className="grid col-start-3 col-end-10 row-start-4 row-end-8">
                     <Dashboard />
                 </div>
                 <div className="grid col-start-4 col-end-10 row-start-1 row-end-4">
                     <Informations />
                 </div>
-                <div className="grid col-start-3 col-end-10 row-start-9 row-end-10">
+                <div className="grid col-start-5 col-end-10 row-start-8 row-end-10">
                     <ActiveEvents />
                 </div>
-                <div className="grid col-start-7 col-end-10 row-start-4 row-end-9">
+                <div className="grid col-start-3 col-end-5 row-start-8 row-end-10">
                 <Texes/>
                 </div>
             </div>
