df9f699b4c82ac9a3a060b35c0ecabe3a0f71d5b
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-09-14 21:47:59 -0300

    adicionado modo tela cheia

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import {CentraldeDadosProvider } from "./centralDeDadosContext";
+import React, { useRef } from "react";
+import { CentraldeDadosProvider } from "./centralDeDadosContext";
 import Interface from "./interface";
 import { DadosEconomyGlobalProvider } from "./dadosEconomyGlobal";
-
 import Notificação from "./notificação";
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -14,7 +14,8 @@ import {
   Tooltip,
   Filler,
   Legend,
-} from 'chart.js';
+} from "chart.js";
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -22,18 +23,41 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-    Filler,
+  Filler,
   Legend
 );
 
 function App() {
+  const containerRef = useRef(null);
+
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      if (containerRef.current.requestFullscreen) {
+        containerRef.current.requestFullscreen();
+      }
+    } else {
+      document.exitFullscreen();
+    }
+  };
+
   return (
     <CentraldeDadosProvider>
       <DadosEconomyGlobalProvider>
+        <div
+          ref={containerRef}
+          className="h-[100vh] w-[100vw] relative bg-gray-900"
+        >
+          {/* Botão de Tela Cheia */}
+          <button
+            onClick={toggleFullscreen}
+            className="absolute top-2 right-2 px-3 py-1 bg-blue-600 text-white rounded-lg hover:bg-blue-700 z-50"
+          >
+            {document.fullscreenElement ? "Sair da Tela Cheia" : "Tela Cheia"}
+          </button>
 
-    <div className="h-[100vh] w-[100vw]">
-      <Interface/>
-    </div>
+          {/* Seu jogo */}
+          <Interface />
+        </div>
       </DadosEconomyGlobalProvider>
     </CentraldeDadosProvider>
   );
