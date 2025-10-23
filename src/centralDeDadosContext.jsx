db5ddb84debf2d226bb1233c879b3bf515ba1172
PauloMiguelVIdal <paulo2.0miguel@gmail.com>
2025-10-22 23:22:34 -0300

    configuração inicial

--- src/centralDeDadosContext.jsx
+++ src/centralDeDadosContext.jsx
@@ -15,7 +15,7 @@ const CentraldeDadosProvider = ({ children }) => {
     fimGame: false,
     nomeEmpresa: "",
     animarCicloDia: null,
-    dia: 1221,
+    dia: 1,
     chanceNovoEvento: 0,
     economiaGlobal: "estável",
     botãoOfertas: "btnNormal",
