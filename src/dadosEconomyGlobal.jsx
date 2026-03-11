a574bb9dff526d0bb91a1ea4cfbdf86854421ce5
PauloMiguelVidal <paulo2.0miguel@gmail.com>
2026-03-11 19:48:19 -0300

    final version corrigido

--- src/dadosEconomyGlobal.jsx
+++ src/dadosEconomyGlobal.jsx
@@ -1094,7 +1094,7 @@ const DadosEconomyGlobalProvider = ({ children }) => {
       //   }
     },
       patrimonioGlobal: 0,
-  valorImpostoAnual: 0,
+
 
       despesasImpostoAnual: {
     diaPagarImpostoAnual: false,
