dd467f482a272a297e258eb780bb18fb1d00f797
PauloMiguelVidal <paulo2.0miguel@gmail.com>
2026-07-10 15:27:18 -0300

    ajustado valores

--- src/dadosEconomyGlobal.jsx
+++ src/dadosEconomyGlobal.jsx
@@ -6,7 +6,7 @@ const DadosEconomyGlobalContext = createContext();
 // Provedor do contexto
 const DadosEconomyGlobalProvider = ({ children }) => {
   const [economiaSetores, setEconomiaSetores] = useState({
-    saldo: 10000000000,
+    saldo: 0,
     fimGame: false,
     economiaGlobal: "estável",
     valorImpostoAnual: 0,
