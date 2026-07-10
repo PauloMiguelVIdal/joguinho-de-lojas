1996c920264ce8c88f94349a66dfbdc89c9f95b0
PauloMiguelVidal <paulo2.0miguel@gmail.com>
2026-07-10 13:54:57 -0300

    alterado valor estoque

--- src/dadosEconomyGlobal.jsx
+++ src/dadosEconomyGlobal.jsx
@@ -6,7 +6,7 @@ const DadosEconomyGlobalContext = createContext();
 // Provedor do contexto
 const DadosEconomyGlobalProvider = ({ children }) => {
   const [economiaSetores, setEconomiaSetores] = useState({
-    saldo: 100000000,
+    saldo: 0,
     fimGame: false,
     economiaGlobal: "estável",
     valorImpostoAnual: 0,
